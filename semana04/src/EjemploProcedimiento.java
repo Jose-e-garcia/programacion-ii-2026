cf806478a02b1f2c749cf3ceba0c1a066f1b0fa4
Jose-e-garcia <jgarcia14@miumg.edu.gt>
2026-08-02 13:13:37 -0600

    ejercicio clase
    no importa

---
+++ semana04/src/EjemploProcedimiento.java
@@ -0,0 +1,18 @@
+public class EjemploProcedimiento {
+    public static void main(String[] args) {
+
+        mostrarSeparador();
+        saludar("David");
+        mostrarSeparador();
+    }
+
+    public static void saludar(String nombre) {
+        System.out.println("Bienvenido/a " + nombre);
+    }
+    public static void mostrarSeparador () {
+        System.out.println("---------------------");
+    }
+
+}
+
+
---
+++ semana04/src/PresupuestoSemanal.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class
+PresupuestoSemanal {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Ingrese su nombre: ");
+        String nombre = entrada.nextLine();
+
+        System.out.print("Ingrese su presupuesto semanal: ");
+        double presupuestoSemanal = entrada.nextDouble();
+
+        System.out.print("Ingrese el gasto de alimentación: ");
+        double gastoAlimentacion = entrada.nextDouble();
+
+        System.out.print("Ingrese el gasto de transporte: ");
+        double gastoTransporte = entrada.nextDouble();
+
+        System.out.print("Ingrese Otros gastos: ");
+        double otrosGastos
+
+
+
+
+
+
+
+
+
+                = entrada.nextDouble();
+
+    }
+
+}
