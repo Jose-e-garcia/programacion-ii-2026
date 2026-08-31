ccd131db39f463ffc4984f24f18d03b8c89ec551
Jose-e-garcia <jgarcia14@miumg.edu.gt>
2026-08-30 20:33:04 -0600

    Clase hija Circulo

--- Semana05-Tarea-Calculo-de-areas-con-POO/src/Figura.java
+++ Semana05-Tarea-Calculo-de-areas-con-POO/src/Figura.java
@@ -1,4 +1,18 @@
 public class Figura {
 
     private String nombre;
+
+    public Figura(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Figura: " + nombre);
+    }
+
+    public abstract double calcularArea();
 }
