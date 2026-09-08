53b14e32c331822159ccc8bd084f87616491903b
Jose-e-garcia <jgarcia14@miumg.edu.gt>
2026-09-07 22:21:11 -0600

    Producto y MainControlCompras creados

---
+++ semana-08-control-compras/src/MainControlCompras.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class MainControlCompras {
+
+    public static void main(String[] args) {
+
+        ArrayList<Producto> productos = new ArrayList<>();
+
+    }
+
+}
